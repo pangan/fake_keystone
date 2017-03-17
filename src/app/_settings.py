cb2d2c41a19d6f9860ffca86e627d6fb18a1130c
pangan <pangan@gmail.com>
2017-03-17 01:02:58 +0100

    changed server name

--- src/app/_settings.py
+++ src/app/_settings.py
@@ -4,7 +4,7 @@ This file is for web application settings
 Author: Amir Mofakhar <pangan@gmail.com>
 """
 
-SERVER_BASE_ADDRESS = 'http://localhost'
+SERVER_BASE_ADDRESS = 'http://url.mofakhar.info'
 SERVER_PORT = 80
 SHORT_ADDRESS_LIFE_TIME_IN_SECOND = 10000
 DATABASE = 'sqlite:///url_shortening.db'
